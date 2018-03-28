e94e64877de8875d48cff1c840892320e6387309
Joakim Kakaei <joabaj88@gmail.com>
2018-03-28 16:02:25 +0100

    first commit

--- main_test.go
+++ main_test.go
@@ -54,7 +54,7 @@ func TestEmptyTable(t *testing.T) {
 	response := executeRequest(req)
 
 	checkResponseCode(t, http.StatusOK, response.Code)
-	// tst
+	//
 	if body := response.Body.String(); body != "[]" {
 		t.Errorf("Expected an empty array. Got %", body)
 	}
